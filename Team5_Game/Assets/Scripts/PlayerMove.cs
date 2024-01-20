c987cff84711f3a3f61b77dc4c6e3d04bd11bc0e
sebin6962 <sebin6962@naver.com>
2024-01-20 23:07:58 +0900

    Player setting

---
+++ Team5_Game/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float speed = 20;
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 dir = new Vector3(h, v, 0);
+
+        transform.position += dir * speed * Time.deltaTime;
+        //print("h:"+ h ",v:" +v);
+        //transform.Translate(Vector3.right * 5 * Time.deltaTime);
+
+        //Vector3 dir = Vector3.right * h + Vector3.up * v;
+       
+    }
+}
