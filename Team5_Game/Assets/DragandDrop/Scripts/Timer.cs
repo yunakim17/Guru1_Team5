0e7e791f0f44d7f72c1ad70596cb8d06d3de4bec
Yuna Kim <euna5657@gmail.com>
2024-01-24 01:37:46 +0900

    타이머 초기 구현

---
+++ Team5_Game/Assets/DragandDrop/Scripts/Timer.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+
+    public static Timer Instance;
+
+    private void Awake()
+    {
+        if (Timer.Instance == null)
+        {
+            Timer.Instance = this;
+        }
+    }
+
+
+    public int totalTime = 7; // 총 시간
+    private int currentTime;   // 현재 시간
+
+    public Text countdownText; //카운트다운하는 텍스트
+
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+
+    public void TimerStart()
+    {
+        gameObject.SetActive(true);
+        currentTime = totalTime;
+        UpdateUI();
+        InvokeRepeating("UpdateCountdown", 1f, 1f); // 1초마다 UpdateCountdown 함수를 호출
+    }
+
+    void UpdateCountdown()
+    {
+        if (currentTime > 0)
+        {
+            currentTime--;
+            UpdateUI();
+        }
+        else
+        {
+            // 시간 종료 처리
+            gameObject.SetActive(false);
+
+            
+        }
+    }
+
+    void UpdateUI()
+    {
+        
+        countdownText.text = currentTime.ToString();
+    }
+
+
+}
